271e7f6f06ffcc4167d39e1fda781077cf722705
Harsh <mandge.harsh1212@gmail.com>
2025-08-06 15:21:54 +0530

    Changes in input component

--- link/src/app/CreatePost/page.tsx
+++ link/src/app/CreatePost/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { use, useState } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import Layout from "@/components/Layout";
--- link/src/components/ui/brutalist-input.tsx
+++ link/src/components/ui/brutalist-input.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-export interface InputProps
-extends React.InputHTMLAttributes<HTMLInputElement> {}
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -18,7 +17,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       />
     );
   }
-);
+)
 Input.displayName = "Input";
 
 export { Input };
